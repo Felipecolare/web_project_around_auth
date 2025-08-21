6caf6c70a27ff153f75484608b9ec9afb5412f4a
felipe <felipe831052@gmail.com>
2025-08-22 00:08:22 +0100

    felipe2025

---
+++ web_project_around_auth-main/src/utils/auth.js
@@ -0,0 +1,29 @@
+export const BASE_URL = "https://se-register-api.en.tripleten-services.com/v1";
+
+export const signin = (email, password) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Content-Security-Policy": "default-src 'self' *.tripleten-service.com",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then((res) => {
+    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  });
+};
+
+export const signup = (email, password) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Content-Security-Policy": "default-src 'self' *.tripleten-service.com",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then((res) => {
+    return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`);
+  });
+};
